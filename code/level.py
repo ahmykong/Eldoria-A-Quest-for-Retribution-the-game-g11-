3ac758b9324a6226499699ff918a33f6fd3d9db1
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-05-05 19:33:39 +0800

    Initialise repository

--- code/level.py
+++ code/level.py
@@ -3,6 +3,8 @@ import pygame
 class Level:
     def __init__(self):
 
+        #TO GET DISPLAY SURFACE
+        self.display_surface = pygame.display.get_surface()
         #for sprite grup setp
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
