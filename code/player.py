113485d825b97479e08d0bbb30d824b45af6ed51
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-05-13 02:21:57 +0800

    adding player movement

--- code/player.py
+++ code/player.py
@@ -6,3 +6,27 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self .direction = pygame.math.Vector2()  #direction for the player to walk
+
+    def input(self):
+        keys = pygame.key.get_pressed  #keyboard input
+
+        if keys[pygame.keys.K_UP]:
+            self.direction.y =-1
+        elif keys[pygame.keys.K_DOWN]:
+            self.direction.y =1
+        else:
+            self.direction.y= 0 #to stop the player from moving in one direction
+
+        if keys[pygame.keys.K_RIGHT]:
+            self.direction.x =1
+        elif keys[pygame.keys.K_LEFT]:
+            self.direction.x =1
+        else:
+            self.direction.x= 0 
+
+    def run(self):
+        #update and draw again
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
