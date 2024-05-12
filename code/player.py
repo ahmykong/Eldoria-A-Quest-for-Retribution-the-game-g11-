5ebe5d8522ceb97ecb535113f83989704043bbc3
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-05-13 02:38:02 +0800

    when player move it will show its coordinates

--- code/player.py
+++ code/player.py
@@ -13,16 +13,16 @@ class Player(pygame.sprite.Sprite):
     def input(self):
         keys = pygame.key.get_pressed()  #keyboard input
 
-        if keys[pygame.keys.K_UP]:
+        if keys[pygame.K_UP]:
             self.direction.y = -1
-        elif keys[pygame.keys.K_DOWN]:
+        elif keys[pygame.K_DOWN]:
             self.direction.y = 1
         else:
             self.direction.y= 0 #to stop the player from moving in one direction
 
-        if keys[pygame.keys.K_RIGHT]:
+        if keys[pygame.K_RIGHT]:
             self.direction.x = 1
-        elif keys[pygame.keys.K_LEFT]:
+        elif keys[pygame.K_LEFT]:
             self.direction.x = -1
         else:
             self.direction.x= 0 
