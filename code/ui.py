be9abc6645898e4dea8a5dbd5a152ca708811ed5
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-05-20 03:08:26 +0800

    start on the ui while waiting for the others

---
+++ code/ui.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import * 
+
+class UI:
+	def _init_(self):
+		
+		# general 
+		self.display_surface = pygame.display.get_surface()
+		self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+
+		# bar setup 
+		self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
+		self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+
+		# convert weapon dictionary
+		self.weapon_graphics = []
+		for weapon in weapon_data.values():
+			path = weapon['graphic']
+			weapon = pygame.image.load(path).convert_alpha()
+			self.weapon_graphics.append(weapon)
+			
+            
