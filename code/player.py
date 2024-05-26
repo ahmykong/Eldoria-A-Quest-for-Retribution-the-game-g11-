43e208c90d5674e70ece7eebac16f685616aa28a
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-05-27 06:16:37 +0800

    add key input for combat movements

--- code/player.py
+++ code/player.py
@@ -1,6 +1,7 @@
 import pygame
 from debug import debug
 from settings import *
+from support import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
@@ -8,6 +9,32 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-26)
+
+        #graphic setup
+        self.import_player_assets()
+        self.status = 'down'
+
+        # movement 
+		self.attacking = False
+		self.attack_cooldown = 400
+		self.attack_time = None
+		self.obstacle_sprites = obstacle_sprites
+
+		# weapon
+		self.create_attack = create_attack
+		self.destroy_attack = destroy_attack
+		self.weapon_index = 0
+		self.weapon = list(weapon_data.keys())[self.weapon_index]
+		self.can_switch_weapon = True
+		self.weapon_switch_time = None
+		self.switch_duration_cooldown = 200
+
+		# magic 
+		self.create_magic = create_magic
+		self.magic_index = 0
+		self.magic = list(magic_data.keys())[self.magic_index]
+		self.can_switch_magic = True
+		self.magic_switch_time = None
 
         #stats for the player
         self.stats = {'health': 100,'energy':60,'attack': 10,'magic': 4,'speed': 5}
@@ -20,6 +47,16 @@ class Player(pygame.sprite.Sprite):
         self.speed = 5
 
         self.obstacle_sprites = obstacle_sprites 
+
+    def import_assets(self):
+        character_path = '../graphics/player/'
+		self.animations = {'up': [],'down': [],'left': [],'right': [],
+			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+			'right_attack':[],'left_attack':[],'up_attack':[],'down_attack':[]}
+
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()  #keyboard input
