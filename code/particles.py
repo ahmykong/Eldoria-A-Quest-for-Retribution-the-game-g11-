3da1b25568f8c184551699c88c9444743b1db9c0
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-06-07 01:29:28 +0800

    confused to why it will run on pc but not on laptop

--- code/particles.py
+++ code/particles.py
@@ -3,7 +3,7 @@ from support import import_folder
 from random import choice
 
 class AnimationPlayer:
-	def _init_(self):
+	def __init__(self):
 		self.frames = {
 			# magic
 			'flame': import_folder('../graphics/particles/flame/frames'),
@@ -42,25 +42,24 @@ class AnimationPlayer:
 	
 	def reflect_images(self,frames):
 		new_frames = []
-		
-		for frame in frames :
-			flipped_frame = pygame.transform.flip(frame,True,False)
-			new_frames.append(flipped_frame)
+
+		for frame in frames:
+	 		flipped_frame = pygame.transform.flip(frame,True,False)
+	 		new_frames.append(flipped_frame)
 		return new_frames
 
-    def create_grass_particles(self,pos,groups):
-	    animation_frames = choice(self.frames['leaf'])
+	def create_grass_particles(self,pos,groups):
+	 	animation_frames = choice(self.frames['leaf'])
 	 	ParticleEffect(pos,animation_frames,groups)
-	
-    
-    def create_particles(self,animation_type,pos,groups):
+
+	def create_particles(self,animation_type,pos,groups):
 		animation_frames = self.frames[animation_type]
 		ParticleEffect(pos,animation_frames,groups)
 
 
 class ParticleEffect(pygame.sprite.Sprite):
-	def _init_(self,pos,animation_frames,groups):
-		super()._init_(groups)
+	def __init__(self,pos,animation_frames,groups):
+		super().__init__(groups)
 		self.sprite_type = 'magic'
 		self.frame_index = 0
 		self.animation_speed = 0.15
