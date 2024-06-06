60f2bf28c1e2e7f4c62df65c9ca36126710ba890
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-06-06 23:43:21 +0800

    pulling syazrin code part 1 enemy

--- code/settings.py
+++ code/settings.py
@@ -36,7 +36,7 @@ magic_data = {
 	'flame': {'strength': 5,'cost': 20,'graphic':'../graphics/particles/flame/fire.png'},
 	'heal' : {'strength': 20,'cost': 10,'graphic':'../graphics/particles/heal/heal.png'}}
 
-# enemy
+# enemy pulled
 monster_data = {
 	'squid': {'health': 100,'exp':100,'damage':20,'attack_type': 'slash', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
 	'raccoon': {'health': 300,'exp':250,'damage':40,'attack_type': 'claw',  'attack_sound':'../audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
