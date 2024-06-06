44f8b45bd2378869acd2a4c7ed6392029d5aa59d
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-06-07 00:10:09 +0800

    making magic.py

---
+++ code/magic.py
@@ -0,0 +1,16 @@
+import pygame
+from settings import *
+from random import randint
+
+class MagicPlayer:
+	def _init_(self,animation_player):
+		self.animation_player = animation_player
+
+	def heal(self,player,strength,cost,groups):
+		if player.energy >= cost:
+			player.health += strength
+			player.energy -= cost
+			if player.health >= player.stats['health']:
+				player.health = player.stats['health']
+			self.animation_player.create_particles('aura',player.rect.center,groups)
+			self.animation_player.create_particles('heal',player.rect.center,groups)
