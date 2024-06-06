ec2f91ad88daf35af160ae25f148561570dd6071
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-06-07 00:08:42 +0800

    fixed or add codes to the entity.py

--- code/entity.py
+++ code/entity.py
@@ -1,8 +1,12 @@
 import pygame
+from math import sin 
 
 class Entity(pygame.sprite.Sprite):
     def _init_(self,group):
         super()._init_(group)
+        self.frame_index = 0
+        self.animation = 0.5
+        self.direction = pygame.math.Vector2()
 
     def move(self,speed):
         if self.direction.magnitude() != 0:
@@ -30,3 +34,10 @@ class Entity(pygame.sprite.Sprite):
                         self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0: #moving up
                         self.hitbox.top = sprite.hitbox.bottom
+                        
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0:
+            return 255
+        else:
+            return 0
