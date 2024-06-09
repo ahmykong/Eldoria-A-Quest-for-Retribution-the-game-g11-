8b08df813792e539aac9438e83b9a03edac856df
Amirul Hilman <1221109328@student.mmu.edu.my>
2024-06-10 02:33:35 +0800

    making start screen

--- code/startscreen.py
+++ code/startscreen.py
@@ -1,6 +1,5 @@
-import pygame, sys
-from settings import *
-from level import Level
+import pygame
+import sys
 
 pygame.init()
 
@@ -8,7 +7,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Eldoria')
+pygame.display.set_caption('MiniIt')
 
 # Colors
 WHITE = (255, 255, 255)
@@ -40,40 +39,3 @@ def start_screen():
 
 if __name__ == "__main__":
     start_screen()
-
-class Game:
-	def __init__(self):
-
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Eldoria')
-		self.clock = pygame.time.Clock()
-
-		self.level = Level()
-
-		# sound 
-		main_sound = pygame.mixer.Sound('../audio/main.ogg')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_m:
-						self.level.toggle_menu()
-
-			self.screen.fill(WATER_COLOR)
-			self.level.run()
-			pygame.display.update()
-			self.clock.tick(FPS)
-
-if __name__ == '__main__':
-	game = Game()
-	game.run()
-
-	
