f08a8f63232725d90700923765113fa811dac62f
JDBeastMY <88874573+JDBeastMY@users.noreply.github.com>
2024-06-21 12:25:39 +0800

    added feature : Weapon , health , energy ect

--- code/level.py
+++ code/level.py
@@ -5,6 +5,8 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
+from ui import UI
 
 class Level:
 	def __init__(self):
@@ -16,32 +18,58 @@ class Level:
 		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
+		# attack sprites
+		self.current_attack = None
+
 		# sprite setup
 		self.create_map()
 
-	def create_map(self):
-	
-		#for style,layout in layouts.items():
-		#	for row_index,row in enumerate(layout):
-			#	for col_index, col in enumerate(row):
-			#		if col != '-1':
-			#			x = col_index * TILESIZE
-			#			y = row_index * TILESIZE
-			#			if style == 'boundary':
-			#				Tile((x,y),[self.obstacle_sprites],'invisible')
-			#			if style == 'grass':
-			#				random_grass_image = choice(graphics['grass'])
-			#				Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
-#
-				#		if style == 'object':
-				#			surf = graphics['objects'][int(col)]
-			#				Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+		# user interface 
+		self.ui = UI()
 
-		self.player = Player((6200,4200),[self.visible_sprites],self.obstacle_sprites)
+	def create_map(self):
+		layouts = {
+			'boundary': import_csv_layout('map/map_FloorBlocks.csv'),
+			'grass': import_csv_layout('map/map_Grass.csv'),
+			'object': import_csv_layout('map/map_Objects.csv'),
+		}
+		graphics = {
+			'grass': import_folder('graphics/Grass'),
+			'objects': import_folder('graphics/objects')
+		}
+
+		for style,layout in layouts.items():
+			for row_index,row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						if style == 'grass':
+							random_grass_image = choice(graphics['grass'])
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
+
+						if style == 'object':
+							surf = graphics['objects'][int(col)]
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+
+		self.player = Player((6000,4000),[self.visible_sprites],self.obstacle_sprites,self.create_attack,self.destroy_attack)
+	
+	def create_attack(self):
+		
+		self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+	def destroy_attack(self):
+		if self.current_attack:
+			self.current_attack.kill()
+		self.current_attack = None
+
 	def run(self):
 		# update and draw the game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
+		self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -55,7 +83,7 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.offset = pygame.math.Vector2()
 
 		# creating the floor
-		self.floor_surf = pygame.image.load('5 - level graphics/graphics/tilemap/MAP.png').convert()
+		self.floor_surf = pygame.image.load('graphics/tilemap/MAP.png').convert()
 		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
 	def custom_draw(self,player):
--- code/player.py
+++ code/player.py
@@ -3,32 +3,46 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups,obstacle_sprites):
+	def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack):
 		super().__init__(groups)
-		self.image = pygame.image.load('graphics/test/Main.png').convert_alpha()
+		self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-26)
 
 		# graphics setup
 		self.import_player_assets()
-		self.status = 'down'
+		self.status = 'bawah'
 		self.frame_index = 0
 		self.animation_speed = 0.15
 
 		# movement 
 		self.direction = pygame.math.Vector2()
-		self.speed = 5
 		self.attacking = False
 		self.attack_cooldown = 400
 		self.attack_time = None
-
 		self.obstacle_sprites = obstacle_sprites
+
+		# weapon
+		self.create_attack = create_attack
+		self.destroy_attack = destroy_attack
+		self.weapon_index = 0
+		self.weapon = list(weapon_data.keys())[self.weapon_index]
+		self.can_switch_weapon = True
+		self.weapon_switch_time = None
+		self.switch_duration_cooldown = 200
+
+		# stats
+		self.stats = {'health': 100,'energy':60,'attack': 10,'magic': 4,'speed': 5}
+		self.health = self.stats['health'] * 0.5
+		self.energy = self.stats['energy'] * 0.8
+		self.exp = 123
+		self.speed = self.stats['speed']
 
 	def import_player_assets(self):
 		character_path = 'graphics/player/'
-		self.animations = {'up': [],'down': [],'left': [],'right': [],
-			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
-			'right_attack':[],'left_attack':[],'up_attack':[],'down_attack':[]}
+		self.animations = {'atas': [],'bawah': [],'kiri': [],'kanan': [],
+			'kanan_idle':[],'kiri_idle':[],'atas_idle':[],'bawah_idle':[],
+			'kanan_attack':[],'kiri_attack':[],'atas_attack':[],'bawah_attack':[]}
 
 		for animation in self.animations.keys():
 			full_path = character_path + animation
@@ -41,19 +55,19 @@ class Player(pygame.sprite.Sprite):
 			# movement input
 			if keys[pygame.K_UP]:
 				self.direction.y = -1
-				self.status = 'up'
+				self.status = 'atas'
 			elif keys[pygame.K_DOWN]:
 				self.direction.y = 1
-				self.status = 'down'
+				self.status = 'bawah'
 			else:
 				self.direction.y = 0
 
 			if keys[pygame.K_RIGHT]:
 				self.direction.x = 1
-				self.status = 'right'
+				self.status = 'kanan'
 			elif keys[pygame.K_LEFT]:
 				self.direction.x = -1
-				self.status = 'left'
+				self.status = 'kiri'
 			else:
 				self.direction.x = 0
 
@@ -61,13 +75,24 @@ class Player(pygame.sprite.Sprite):
 			if keys[pygame.K_SPACE]:
 				self.attacking = True
 				self.attack_time = pygame.time.get_ticks()
-				print('attack')
+				self.create_attack()
 
 			# magic input 
 			if keys[pygame.K_LCTRL]:
 				self.attacking = True
 				self.attack_time = pygame.time.get_ticks()
 				print('magic')
+
+			if keys[pygame.K_q] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				
+				if self.weapon_index < len(list(weapon_data.keys())) - 1:
+					self.weapon_index += 1
+				else:
+					self.weapon_index = 0
+					
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
 
 	def get_status(self):
 
@@ -121,6 +146,11 @@ class Player(pygame.sprite.Sprite):
 		if self.attacking:
 			if current_time - self.attack_time >= self.attack_cooldown:
 				self.attacking = False
+				self.destroy_attack()
+
+		if not self.can_switch_weapon:
+			if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+				self.can_switch_weapon = True
 
 	def animate(self):
 		animation = self.animations[self.status]
